7390ba4e74c5c29e53150e18ce572a4dba828269
Rippey Fam <rippey.fam@gmail.com>
2025-12-22 16:48:42 -0700

    Customized my library app

--- express-locallibrary-tutorial/routes/users.js
+++ express-locallibrary-tutorial/routes/users.js
@@ -1,9 +1,18 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get("/", function (req, res, next) {
+  res.send("respond with a resource");
+});
+
+router.get("/:username", (req, res) => {
+  let username = req.params.username;
+  username = username.toLowerCase();
+  username = username.split("");
+  username[0] = username[0].toUpperCase();
+  username = username.join("");
+  res.render("cool", { title: username });
 });
 
 module.exports = router;
