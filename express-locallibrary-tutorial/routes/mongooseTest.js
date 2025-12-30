230da90b59d9d7575dc80847398866bd25bed181
Rippey Fam <rippey.fam@gmail.com>
2025-12-30 14:00:19 -0700

    Added MongoDB

--- express-locallibrary-tutorial/routes/mongooseTest.js
+++ express-locallibrary-tutorial/routes/mongooseTest.js
@@ -2,18 +2,42 @@ const express = require("express");
 const mongoose = require("mongoose");
 const router = express.Router();
 
-/* GET home page. */
-router.get("/", function (req, res, next) {
-  res.render("mongooseTest", {
-    data: [
-      {
-        Name: "Matthew Rippey",
-        Age: 14,
-        "Favorite Language": "JavaScript",
-        "Favorite Food": "Sushi",
-      },
-    ],
-  });
+// Set `strictQuery: false` to globally opt into filtering by properties that aren't in the schema
+// Included because it removes preparatory warnings for Mongoose 7.
+// See: https://mongoosejs.com/docs/migrating_to_6.html#strictquery-is-removed-and-replaced-by-strict
+mongoose.set("strictQuery", false);
+
+// Define the database URL to connect to.
+const mongoDB = "mongodb://127.0.0.1/my_database";
+
+// Wait for database to connect, logging an error if there is a problem
+router.get("/", async function (req, res, next) {
+  try {
+    await mongoose.connect(mongoDB);
+    res.render("mongooseTest", {
+      data: [
+        {
+          Name: "Matthew Rippey",
+          Age: 14,
+          "Favorite Language": "JavaScript",
+          "Favorite Food": "Sushi",
+        },
+        { "Connected to Mongoose?": "YES!" },
+      ],
+    });
+  } catch (err) {
+    res.render("mongooseTest", {
+      data: [
+        {
+          Name: "Matthew Rippey",
+          Age: 14,
+          "Favorite Language": "JavaScript",
+          "Favorite Food": "Sushi",
+        },
+        { "Connected to Mongoose?": "NO" },
+      ],
+    });
+  }
 });
 
 module.exports = router;
