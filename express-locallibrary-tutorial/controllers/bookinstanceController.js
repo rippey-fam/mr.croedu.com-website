4b67dc69f71085546c47573cb57c3905d5e69bcd
Rippey Fam <rippey.fam@gmail.com>
2026-01-20 15:12:36 -0700

    Added delete pages

--- express-locallibrary-tutorial/controllers/bookinstanceController.js
+++ express-locallibrary-tutorial/controllers/bookinstanceController.js
@@ -1,4 +1,6 @@
 const BookInstance = require("../models/bookinstance");
+const Book = require("../models/book");
+const { body, validationResult } = require("express-validator");
 
 // Display list of all BookInstances.
 exports.bookinstance_list = async (req, res, next) => {
@@ -31,22 +33,83 @@ exports.bookinstance_detail = async (req, res, next) => {
 
 // Display BookInstance create form on GET.
 exports.bookinstance_create_get = async (req, res, next) => {
-  res.send("NOT IMPLEMENTED: BookInstance create GET");
+  const allBooks = await Book.find({}, "title").sort({ title: 1 }).exec();
+
+  res.render("bookinstance_form", {
+    title: "Create BookInstance",
+    book_list: allBooks,
+  });
 };
 
 // Handle BookInstance create on POST.
-exports.bookinstance_create_post = async (req, res, next) => {
-  res.send("NOT IMPLEMENTED: BookInstance create POST");
-};
+exports.bookinstance_create_post = [
+  // Validate and sanitize fields.
+  body("book", "Book must be specified").trim().isLength({ min: 1 }).escape(),
+  body("imprint", "Imprint must be specified")
+    .trim()
+    .isLength({ min: 1 })
+    .escape(),
+  body("status").escape(),
+  body("due_back", "Invalid date")
+    .optional({ values: "falsy" })
+    .isISO8601()
+    .toDate(),
 
-// Display BookInstance delete form on GET.
+  // Process request after validation and sanitization.
+  async (req, res, next) => {
+    // Extract the validation errors from a request.
+    const errors = validationResult(req);
+
+    // Create a BookInstance object with escaped and trimmed data.
+    const bookInstance = new BookInstance({
+      book: req.body.book,
+      imprint: req.body.imprint,
+      status: req.body.status,
+      due_back: req.body.due_back,
+    });
+
+    if (!errors.isEmpty()) {
+      // There are errors.
+      // Render form again with sanitized values and error messages.
+      const allBooks = await Book.find({}, "title").sort({ title: 1 }).exec();
+
+      res.render("bookinstance_form", {
+        title: "Create BookInstance",
+        book_list: allBooks,
+        selected_book: bookInstance.book._id,
+        errors: errors.array(),
+        bookinstance: bookInstance,
+      });
+      return;
+    }
+
+    // Data from form is valid
+    await bookInstance.save();
+    res.redirect(bookInstance.url);
+  },
+];
+
+// Display bookinstance delete form on GET.
 exports.bookinstance_delete_get = async (req, res, next) => {
-  res.send("NOT IMPLEMENTED: BookInstance delete GET");
+  // Get details of bookinstance and all their (in parallel)
+  const bookinstance = await BookInstance.findById(req.params.id).exec();
+
+  if (bookinstance === null) {
+    // No results.
+    res.redirect("/catalog/books");
+    return;
+  }
+
+  res.render("bookinstance_delete", {
+    title: "Delete Book Instance",
+    bookinstance,
+  });
 };
 
-// Handle BookInstance delete on POST.
+// Handle book delete on POST.
 exports.bookinstance_delete_post = async (req, res, next) => {
-  res.send("NOT IMPLEMENTED: BookInstance delete POST");
+  await BookInstance.findByIdAndDelete(req.body.bookinstanceid);
+  res.redirect("/catalog/bookinstances");
 };
 
 // Display BookInstance update form on GET.
