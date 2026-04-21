a7c85f7283ce93d9f9573e8fa2907874932af832
Rippey Fam <rippey.fam@gmail.com>
2026-04-21 15:09:29 -0600

    Added Update Functionality

--- todo-react-app/frontend-react/UpdateItem.tsx
+++ todo-react-app/frontend-react/UpdateItem.tsx
@@ -1,20 +1,66 @@
-import { Link, useParams } from "react-router-dom";
+import { Link, useNavigate, useParams } from "react-router-dom";
 import { useTodoListContext } from "./useTodoList";
+import { useEffect, useState } from "react";
 
 export default function UpdateItem() {
   const { id } = useParams();
   const { list, updateItem } = useTodoListContext();
   const todoitem = list.find((item) => item._id === id);
+  const navigate = useNavigate();
+  console.log("todoitem:", todoitem);
+  const [formVals, setFormVals] = useState({
+    title: "Title",
+    due_date: "",
+  });
+
+  useEffect(() => {
+    if (todoitem) {
+      setFormVals({
+        title: todoitem.title,
+        due_date: todoitem.due_date
+          ? new Date(todoitem.due_date).toISOString().split("T")[0]
+          : "",
+      });
+    }
+  }, [todoitem]);
+
   return (
     <div className="center-block">
-      <h1> Edit todo</h1>
-      <p>{id}</p>
+      <h1>Edit todo</h1>
+      <p>
+        {todoitem ? todoitem.title + ":" : ""} {id}
+      </p>
       <form>
         <label>Todo Title</label>
-        <input type="text" value={todoitem?.title} />
+        <input
+          type="text"
+          value={formVals.title}
+          onChange={(e) => setFormVals({ ...formVals, title: e.target.value })}
+        />
         <label>Due Date</label>
-        <input type="date"></input>
-        <br />
+        <input
+          type="date"
+          value={formVals.due_date}
+          onChange={(e) =>
+            setFormVals({ ...formVals, due_date: e.target.value })
+          }
+        />
+        <button
+          onClick={() => {
+            if (!todoitem) {
+              return;
+            }
+            updateItem(todoitem, {
+              title: formVals.title,
+              due_date: formVals.due_date
+                ? new Date(formVals.due_date)
+                : undefined,
+            });
+            navigate("/");
+          }}
+        >
+          Submit
+        </button>
       </form>
       <Link
         to="/"
